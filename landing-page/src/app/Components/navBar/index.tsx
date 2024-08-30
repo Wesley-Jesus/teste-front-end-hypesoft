57dcbfcb0d383f416e933e720cd17343e65171b4
Wesley Jesus <wesleyjesus.b@gmail.com>
2024-08-30 16:02:43 -0300

    feat: criacao My Service

--- landing-page/src/app/Components/navBar/index.tsx
+++ landing-page/src/app/Components/navBar/index.tsx
@@ -1,21 +1,32 @@
 import styles from "./NavBar.module.css";
+import Image from "next/image";
+
 
 const NavBar = () => (
-    <nav className={`${styles.nav} container`}>
-      <ul className={styles.lisNav}>
-        <li>
-          <a href="#about">ABOUT</a>
-        </li>
-        <li>
-          <a href="#service">SERVICE</a>
-        </li>
-        <li>
-          <a href="#works">WORKS</a>
-        </li>
-        <li>
-          <a href="#contact">CONTACT</a>
-        </li>
-      </ul>
-    </nav>
+  <nav className={styles.nav}>
+    <Image
+              src="public\images\about_image2.svg"
+              alt="Vercel Logo"
+              // className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+    <ul
+    className={styles.lisNav}>
+      <li>
+        <a href="#about">ABOUT</a>
+      </li>
+      <li>
+        <a href="#service">SERVICE</a>
+      </li>
+      <li>
+        <a href="#works">WORKS</a>
+      </li>
+      <li>
+        <a href="#contact">CONTACT</a>
+      </li>
+    </ul>
+  </nav>
 );
 export default NavBar;
--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -1,8 +1,14 @@
+import Abaout from "./Components/About/About";
+import Myservice from "./Components/Mysevice";
+import NavBar from "./Components/navBar";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <NavBar />
+      <Abaout />
+      <Myservice />
     </main>
   );
 }
